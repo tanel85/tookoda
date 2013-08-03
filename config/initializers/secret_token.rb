43d5210682a86aeb5fb6da558f21f57ab988006c
unknown <tanelt@DELL305.webmedia.int>
2013-08-03 14:04:23 +0100

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tookoda::Application.config.secret_token = 'd673e929ba74cf3d0e86d37449ea5c580a299a6e748adfd42768508210fb0f7f927dd0f2694316a61a8ba8c4ed26b17eeeb4b18fdf4697e6fb1ac1fee1e0451c'
