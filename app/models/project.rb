e05ea86a46d267c1c2ee8e26a93e34b063dcb2d9
unknown <tanelt@laptop626.webmedia.int>
2014-02-24 12:50:20 +0200

    projekti nimi exceli nimeks

--- app/models/project.rb
+++ app/models/project.rb
@@ -7,4 +7,8 @@ class Project < ActiveRecord::Base
   has_many :contamination_sources, dependent: :destroy
   has_many :project_chemicals, dependent: :destroy
   
+  def self.find_project_name id
+    Project.where(:id => id).pluck(:name)[0]
+  end
+  
 end
